1796805a5d5608cb5b23d03ccce0eecaf3583cb2
googlesheets <rtk3rns@gmail.com>
2021-07-02 09:47:58 -0400

    Bump version: 0.5.11 → 0.5.12

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     packages=find_packages(include=["mixmasta", "mixmasta.*"]),
     test_suite="tests",
     url="https://github.com/jataware/mixmasta",
-    version='0.5.11',
+    version='0.5.12',
     zip_safe=False,
 )
