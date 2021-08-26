491451ef64210e543e47e9d4fe3e253a79007fc2
Brandon Rose <rose.brandon.m@gmail.com>
2021-08-25 21:45:39 -0500

    Bump version: 0.5.19 → 0.5.20

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     packages=find_packages(include=["mixmasta", "mixmasta.*"]),
     test_suite="tests",
     url="https://github.com/jataware/mixmasta",
-    version='0.5.19',
+    version='0.5.20',
     zip_safe=False,
 )
